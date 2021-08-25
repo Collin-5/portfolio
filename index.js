17923cc2d2055c65f386f95eeac9c97769af5045
Collin-5 <collinsotieno380@gmail.com>
2021-08-25 22:15:41 +0300

    all

---
+++ index.js
@@ -0,0 +1,87 @@
+const center = document.querySelector(".center");
+const topLink = document.querySelector(".totop");
+const button = document.querySelector(".toggle");
+const navcontainer = document.querySelector(".nav-list");
+
+
+window.addEventListener('scroll', function(){
+    const scrollheight = window.pageYOffset;
+    const navheight = center.getBoundingClientRect().height;
+    if (scrollheight > navheight){
+        center.classList.add("fixed");
+    }
+    else {
+        center.classList.remove("fixed");
+    }
+    if (scrollheight > 600){
+        topLink.classList.remove("totop-hide");
+    }
+    else {
+        topLink.classList.add("totop-hide");
+    }
+    navcontainer.classList.add("hidden");
+})
+
+button.addEventListener('click', function(){
+    navcontainer.classList.toggle("hidden");
+})
+
+// ********** smooth scroll ************
+// select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+
+    const navHeight = center.getBoundingClientRect().height;
+    const containerHeight = navcontainer.getBoundingClientRect().height;
+    const fixedNav = center.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+
+    if (!fixedNav) {
+      position = position - navHeight;
+    }
+    if (navHeight > 82) {
+      position = position + containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+    });
+    // close
+    navcontainer.style.height = 0;
+  });
+});
+
+// preloader
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener('load', function(){
+  preloader.classList.add("hide-preloader");
+})
+
+// about
+const btns = document.querySelectorAll(".bt");
+const about = document.querySelectorAll(".about");
+const contents = document.querySelectorAll(".content");
+
+window.addEventListener('click', function(e){
+  const id =e.target.dataset.id;
+  if (id){
+    btns.forEach(function(btn){
+      btn.classList.remove("active");
+      e.target.classList.add("active");
+
+    })
+    contents.forEach(function(content){
+      content.classList.remove("active");
+    })
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+})
